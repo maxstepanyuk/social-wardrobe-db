3cb14dc54f34c558239bdb6065736005dd27dba3
Maks Step - Deb pc <40894200+maxstepanyuk@users.noreply.github.com>
2024-03-16 09:49:33 +0200

    copied select queries for working with data and for creating triggers from Uni HW

---
+++ ПІС ПР 03 - smart wardrobe postgresql - queries.sql
@@ -0,0 +1,296 @@
+--postgresql
+
+
+-- 1. + простий запит на вибірку з однієї таблиці;
+SELECT
+	*
+FROM
+	outfits o 
+;
+
+-- -- 2. простий запит на вибірку з декількох таблиць
+-- users and their gender (from profile.gender_id and gender.name)
+
+SELECT
+	u.user_id id,
+	u.username,
+	p.biography,
+	p.birthdate,
+	g.name AS gender
+FROM
+	"user" u
+JOIN "profile" p 
+	ON
+	u.user_id = p.user_id
+JOIN "gender" g 
+	ON
+	p.gender_id = g.gender_id
+ORDER BY
+	u.user_id 
+;
+
+-- всі образи разом з усіма предметами одягу
+SELECT 
+	outfits.outfit_id, 
+	outfits.name, 
+	garment.garment_id, 
+	gt.name
+FROM
+	outfits
+JOIN garments_outfits 
+	ON
+	outfits.outfit_id = garments_outfits.outfit_id
+JOIN garment 
+	ON
+	garments_outfits.garment_id = garment .garment_id
+JOIN garment_type gt 
+	ON
+	garment .garment_type_id = gt.garment_type_id
+;
+
+
+
+-- -- 3. запит з групуванням;
+
+-- кількості відгуків на кожний образ
+
+SELECT
+	o.outfit_id,
+	o."name" AS "outfit name",
+	COUNT(c.comment_id) AS total_comments
+FROM
+	outfits o
+JOIN "comments" as c ON
+	o.outfit_id = c.outfit_id
+GROUP BY
+	o.outfit_id
+ORDER BY total_comments DESC 
+;
+
+
+-- -- 4. запит з обчислювальним полем;
+-- підрахунок віку користувачів на основі їхніх дат народження
+-- функція AGE - різниця між двома датами
+SELECT
+	p.user_id ,
+	p.birthdate,
+	AGE(CURRENT_DATE, p.birthdate) AS age
+FROM
+	profile p 
+ORDER
+	BY birthdate DESC 
+;
+
+-- показати тільки роки
+SELECT
+	user_id ,
+	DATE_PART('year', AGE(CURRENT_DATE, p.birthdate)) AS age
+FROM
+	profile p 
+;
+
+-- -- 5. запит з використанням агрегатних функцій;
+-- знаходження середньої оцінки для кожного образу
+
+SELECT
+	outfit_id,
+	AVG(rating) AS avg_rating
+FROM
+	ratings
+GROUP BY
+	outfit_id
+ORDER BY avg_rating DESC 
+;
+
+-- 6. запит з сортуванням;
+
+SELECT
+	user_id,
+	username,
+	registration_date
+FROM
+	"user" u
+ORDER BY
+	u.registration_date 
+;
+
+SELECT
+	outfit_id ,
+	"name" ,
+	date_created ,
+	date_worn
+FROM
+	outfits
+ORDER BY
+	date_worn DESC
+;
+
+
+-- 7. запит з використанням умови;
+
+SELECT
+	o.outfit_id ,
+	o."name" ,
+	o.date_worn
+FROM
+	outfits o
+WHERE
+	AGE('2024-03-02'::date ,
+	date_worn) <= '50 days'
+;
+
+
+SELECT
+	o.outfit_id ,
+	o."name" ,
+	o.date_worn,
+	AGE(CURRENT_DATE,
+	date_worn) AS ago
+FROM
+	outfits o
+WHERE
+	AGE(CURRENT_DATE,
+	date_worn) <= '50 days'
+;
+
+
+
+-- 8. запит з використанням підзапитів.
+
+-- вибірка образів, які містять певну категорію одягу
+
+SELECT
+	*
+FROM
+	outfits
+WHERE
+	outfit_id 
+IN 
+(
+	SELECT
+		outfit_id
+	FROM
+		garments_outfits
+	WHERE
+		garment_id 
+	IN 
+	(
+		SELECT
+			garment_id
+		FROM
+			garment
+		WHERE
+			garment_type_id 
+		IN (
+			SELECT
+				garment_type_id
+			FROM
+				garment_type gt
+			WHERE 
+				gt."name" = 'Shirt'
+		)
+	)
+)
+;
+
+
+-- Створити 3 тригера (наприклад, для запису в журнал подій операцій з базою даних).
+
+-- on delete - user table
+CREATE OR REPLACE FUNCTION user_delete_log()
+RETURNS TRIGGER AS $user_delete$
+BEGIN
+    INSERT INTO action_log (table_name, row_id, action)
+    VALUES ('user', OLD.user_id, 'delete');
+    RETURN OLD;
+END;
+$user_delete$ LANGUAGE plpgsql;
+
+CREATE TRIGGER user_delete_trigger
+AFTER DELETE ON "user"
+FOR EACH ROW
+EXECUTE FUNCTION user_delete_log();
+
+-- outfit
+CREATE OR REPLACE FUNCTION outfit_delete_log()
+RETURNS TRIGGER AS $outfit_delete$
+BEGIN
+    INSERT INTO action_log (table_name, row_id, action)
+    VALUES ('outfit', OLD.outfit_id, 'delete');
+    RETURN OLD;
+END;
+$outfit_delete$ LANGUAGE plpgsql;
+
+CREATE TRIGGER outfit_delete_trigger
+AFTER DELETE ON outfits
+FOR EACH ROW
+EXECUTE FUNCTION outfit_delete_log();
+
+-- 
+CREATE OR REPLACE FUNCTION comment_delete_log()
+RETURNS TRIGGER AS $comment_delete$
+BEGIN
+    INSERT INTO action_log (table_name, row_id, action)
+    VALUES ('comment', OLD.comment_id, 'delete');
+    RETURN OLD;
+END;
+$comment_delete$ LANGUAGE plpgsql;
+
+CREATE TRIGGER comment_delete_trigger
+AFTER DELETE ON comments
+FOR EACH ROW
+EXECUTE FUNCTION comment_delete_log();
+
+-- test trigger
+SELECT * FROM "comments" c ;
+DELETE FROM "comments" c WHERE "comment_id" = 3;
+
+SELECT * FROM action_log al ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- list all DBs
+SELECT datname FROM pg_database; 
+
+
+-- list all tables brom this db
+SELECT
+	table_name
+FROM
+	information_schema.tables
+WHERE
+	table_schema = 'public'
+	AND table_type = 'BASE TABLE';
+
+
+
+
+
+-- column information from a table
+SELECT
+	column_name,
+	data_type,
+	column_default ,
+	is_nullable ,
+	udt_name
+FROM
+	information_schema.columns
+WHERE
+	table_name = 'comments'
+;

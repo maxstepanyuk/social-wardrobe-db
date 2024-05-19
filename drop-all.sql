a20c467d30f577032459bf6e81cba766858a8019
Maks Step - Deb pc <40894200+maxstepanyuk@users.noreply.github.com>
2024-05-19 21:26:17 +0300

    add drop-all.sql

---
+++ drop-all.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS auth."users"; -- local psql 
+
+DROP TABLE IF EXISTS public."comments";
+DROP TABLE IF EXISTS public."ratings";
+DROP TABLE IF EXISTS public."followers";
+DROP TABLE IF EXISTS public."garments_outfits";
+DROP TABLE IF EXISTS public."outfits";
+DROP TABLE IF EXISTS public."outfit_type";
+DROP TABLE IF EXISTS public."garment";
+DROP TABLE IF EXISTS public."garment_type";
+DROP TABLE IF EXISTS public."profile";
+DROP TABLE IF EXISTS public."gender";
+DROP TABLE IF EXISTS public."user";
+
+DROP TABLE IF EXISTS public."action_log";

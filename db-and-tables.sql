21f267ba248263dee12e2d5eff8426ca5493fdde
Maks Step - Deb pc <40894200+maxstepanyuk@users.noreply.github.com>
2024-05-19 14:17:49 +0300

    add .public schema when referencing tables

--- db-and-tables.sql
+++ db-and-tables.sql
@@ -13,7 +13,7 @@
 -- \c wardrobe; 
 
 -- user profile gender
-CREATE TABLE IF NOT EXISTS "user" (
+CREATE TABLE IF NOT EXISTS public."user" (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
@@ -21,38 +21,38 @@ CREATE TABLE IF NOT EXISTS "user" (
     registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS "gender" (
+CREATE TABLE IF NOT EXISTS public."gender" (
     gender_id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "profile" (
+CREATE TABLE IF NOT EXISTS public."profile" (
     profile_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (user_id) REFERENCES public."user"(user_id),
     birthdate DATE,
     contact VARCHAR(255),
     address VARCHAR(255),
     gender_id INT,
-    FOREIGN KEY (gender_id) REFERENCES gender(gender_id),
+    FOREIGN KEY (gender_id) REFERENCES public.gender(gender_id),
     biography TEXT,
     avatar VARCHAR(255)
 );
 
 -- garments and types
 
-CREATE TABLE IF NOT EXISTS garment_type (
+CREATE TABLE IF NOT EXISTS public."garment_type" (
     garment_type_id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE
 -- TODO - add per user defined type??
 );
 
-CREATE TABLE IF NOT EXISTS garment (
+CREATE TABLE IF NOT EXISTS public."garment" (
     garment_id SERIAL PRIMARY KEY,
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (user_id) REFERENCES public."user"(user_id),
     garment_type_id INT,
-    FOREIGN KEY (garment_type_id) REFERENCES garment_type(garment_type_id),
+    FOREIGN KEY (garment_type_id) REFERENCES public.garment_type(garment_type_id),
     photo VARCHAR(255),
     date_worn DATE,
     date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -60,19 +60,20 @@ CREATE TABLE IF NOT EXISTS garment (
 
 -- outfits and types
 
-CREATE TABLE IF NOT EXISTS outfit_type (
+CREATE TABLE IF NOT EXISTS public."outfit_type" (
     outfit_type_id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE
 -- TODO - add per user defined type??
 );
 
-CREATE TABLE IF NOT EXISTS outfits (
+
+CREATE TABLE IF NOT EXISTS public."outfits" (
     outfit_id SERIAL PRIMARY KEY,
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (user_id) REFERENCES public."user"(user_id),
     name VARCHAR(255),
     outfit_type_id INT,
-    FOREIGN KEY (outfit_type_id) REFERENCES outfit_type(outfit_type_id),
+    FOREIGN KEY (outfit_type_id) REFERENCES public.outfit_type(outfit_type_id),
     date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_edited TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     date_worn DATE,
@@ -81,45 +82,45 @@ CREATE TABLE IF NOT EXISTS outfits (
 
 -- garments (m-m) outfits
 
-CREATE TABLE IF NOT EXISTS garments_outfits (
+CREATE TABLE IF NOT EXISTS public."garments_outfits" (
     garment_id INT,
-    FOREIGN KEY (garment_id) REFERENCES garment(garment_id),
+    FOREIGN KEY (garment_id) REFERENCES public.garment(garment_id),
     outfit_id INT,
-    FOREIGN KEY (outfit_id) REFERENCES outfits(outfit_id)
+    FOREIGN KEY (outfit_id) REFERENCES public.outfits(outfit_id)
 );
 
 -- followers ratings comments
 
-CREATE TABLE IF NOT EXISTS followers (
+CREATE TABLE IF NOT EXISTS public."followers" (
     follower_id INT,
-    FOREIGN KEY (follower_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (follower_id) REFERENCES public."user"(user_id),
     following_id INT,
-    FOREIGN KEY (following_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (following_id) REFERENCES public."user"(user_id),
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS ratings (
+CREATE TABLE IF NOT EXISTS public."ratings" (
     outfit_id INT,
-    FOREIGN KEY (outfit_id) REFERENCES outfits(outfit_id),
+    FOREIGN KEY (outfit_id) REFERENCES public.outfits(outfit_id),
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (user_id) REFERENCES public."user"(user_id),
     rating INT,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS comments (
+CREATE TABLE IF NOT EXISTS public."comments" (
     comment_id SERIAL PRIMARY KEY,
     outfit_id INT,
-    FOREIGN KEY (outfit_id) REFERENCES outfits(outfit_id),
+    FOREIGN KEY (outfit_id) REFERENCES public.outfits(outfit_id),
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES "user"(user_id),
+    FOREIGN KEY (user_id) REFERENCES public."user"(user_id),
     comment TEXT,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
 -- logs
-CREATE TABLE action_log (
+CREATE TABLE public."action_log" (
     log_id SERIAL PRIMARY KEY,
     table_name VARCHAR(255) NOT NULL,
     row_id INT NOT NULL,

588219b4c015623a45b3381c6e2d9369c69d2ed6
Maks Step - Deb pc <40894200+maxstepanyuk@users.noreply.github.com>
2024-05-21 20:36:00 +0300

    upd test data for new tables

--- data.sql
+++ data.sql
@@ -5,53 +5,86 @@
 
 -- data
 
-INSERT INTO "user" (username, email, password) VALUES
-('user1', 'user1@example.com', 'password1'),
-('user2', 'user2@example.com', 'password2'),
-('user3', 'user3@example.com', 'password3'),
-('user4', 'user4@example.com', 'password4'),
-('user5', 'user5@example.com', 'password5');
+INSERT INTO auth."users" (id, email, password) VALUES
+('c068fec8-70f7-4d3e-88eb-395c5aed4e44','user1@example.com', 'password1'),
+('7b75bd54-0e55-43e1-a2d9-20024c80fd9a','user2@example.com', 'password2'),
+('692a512a-7736-4651-a1ae-3cc8eba08ffa','user3@example.com', 'password3'),
+('d31c712f-88d7-4723-97b3-0526f8de39ea','user4@example.com', 'password4'),
+('9f41b163-8264-4adc-8ad0-9d4cc8d0282b','user5@example.com', 'password5');
 
-INSERT INTO "user" (username, email, password, registration_date) VALUES 
-('john_doe', 'john@example.com', 'password1', '2024-01-01 08:00:00'),
-('jane_smith', 'jane@example.com', 'password2', '2024-01-02 09:00:00'),
-('alex_jones', 'alex@example.com', 'password3', '2024-01-03 10:00:00'),
-('emily_wilson', 'emily@example.com', 'password4', '2024-01-04 11:00:00'),
-('michael_clark', 'michael@example.com', 'password5', '2024-01-05 12:00:00'),
-('sarah_brown', 'sarah@example.com', 'password6', '2024-01-06 13:00:00'),
-('david_thomas', 'david@example.com', 'password7', '2024-01-07 14:00:00'),
-('laura_martin', 'laura@example.com', 'password8', '2024-01-08 15:00:00'),
-('chris_hill', 'chris@example.com', 'password9', '2024-01-09 16:00:00'),
-('jessica_scott', 'jessica@example.com', 'password10', '2024-01-10 17:00:00'),
-('matt_wright', 'matt@example.com', 'password11', '2024-01-11 18:00:00'),
-('olivia_green', 'olivia@example.com', 'password12', '2024-01-12 19:00:00'),
-('ryan_evans', 'ryan@example.com', 'password13', '2024-01-13 20:00:00'),
-('hannah_king', 'hannah@example.com', 'password14', '2024-01-14 21:00:00'),
-('tom_hall', 'tom@example.com', 'password15', '2024-01-15 22:00:00'),
-('lucy_adams', 'lucy@example.com', 'password16', '2024-01-16 23:00:00'),
-('peter_wilson', 'peter@example.com', 'password17', '2024-01-17 00:00:00'),
-('rachel_baker', 'rachel@example.com', 'password18', '2024-01-18 01:00:00'),
-('jack_carter', 'jack@example.com', 'password19', '2024-01-19 02:00:00'),
-('amy_morris', 'amy@example.com', 'password20', '2024-01-20 03:00:00');
+INSERT INTO auth."users" (id, email, password, registration_date) VALUES 
+('15e23f5a-bebe-4764-9e7b-d8f2c38ce8bf','john@example.com', 'password1', '2024-01-01 08:00:00'),
+('25e23f5a-bebe-4764-9e7b-d8f2c38ce8b1','jane@example.com', 'password2', '2024-01-02 09:00:00'),
+('35e23f5a-bebe-4764-9e7b-d8f2c38ce8b2','alex@example.com', 'password3', '2024-01-03 10:00:00'),
+('faf6f7f9-d337-4680-9e9f-91a4e225586c','emily@example.com', 'password4', '2024-01-04 11:00:00'),
+('04c92d1c-bbfa-48d4-a18f-9b6bb8af4f37','michael@example.com', 'password5', '2024-01-05 12:00:00'),
+('d276adce-71e6-4aff-8476-e539704e4fb1','sarah@example.com', 'password6', '2024-01-06 13:00:00'),
+('afb7055b-184b-41d7-88ae-ac7367cfb6cf','david@example.com', 'password7', '2024-01-07 14:00:00'),
+('c20cf0e4-8132-4384-88fc-1c1a3c2eb6c1','laura@example.com', 'password8', '2024-01-08 15:00:00'),
+('9ab47964-4b0a-4b17-87dd-965aa38ed718','chris@example.com', 'password9', '2024-01-09 16:00:00'),
+('e3cff5c4-caa2-4fc0-9d56-87131d551751','jessica@example.com', 'password10', '2024-01-10 17:00:00'),
+('d1338087-d0a3-47e4-9b89-99bf29e4212b','matt@example.com', 'password11', '2024-01-11 18:00:00'),
+('942300f3-bbe4-489a-bf1c-72068ef15d2c','olivia@example.com', 'password12', '2024-01-12 19:00:00'),
+('6580061d-3c76-446e-9440-b47a472c1d90','ryan@example.com', 'password13', '2024-01-13 20:00:00'),
+('aade44df-1833-41f9-a038-c94b3bb94caa','hannah@example.com', 'password14', '2024-01-14 21:00:00'),
+('0941585d-8e20-4612-a26f-b68755034baf','tom@example.com', 'password15', '2024-01-15 22:00:00'),
+('1e959812-61bc-4ec0-bacb-a3863f8f9347','lucy@example.com', 'password16', '2024-01-16 23:00:00'),
+('13223fea-de83-4f3e-aa26-02e5b30bdafc','peter@example.com', 'password17', '2024-01-17 00:00:00'),
+('7445eaee-d1fb-49a2-96d0-ed10bcd59fc8','rachel@example.com', 'password18', '2024-01-18 01:00:00'),
+('d5a22087-8663-4798-a3da-812bbb324d84','jack@example.com', 'password19', '2024-01-19 02:00:00'),
+('220847b2-ea52-4dfa-828e-c644409c8e36','amy@example.com', 'password20', '2024-01-20 03:00:00');
 
-SELECT * FROM "user";
+SELECT * FROM auth."users";
 
-INSERT INTO "gender" (name) VALUES
+
+INSERT INTO public."user" (supabaseauth_user_id, username) VALUES
+('c068fec8-70f7-4d3e-88eb-395c5aed4e44','user1'),
+('7b75bd54-0e55-43e1-a2d9-20024c80fd9a','user2'),
+('692a512a-7736-4651-a1ae-3cc8eba08ffa','user3'),
+('d31c712f-88d7-4723-97b3-0526f8de39ea','user4'),
+('9f41b163-8264-4adc-8ad0-9d4cc8d0282b','user5');
+
+INSERT INTO "user" (supabaseauth_user_id, username) VALUES 
+('15e23f5a-bebe-4764-9e7b-d8f2c38ce8bf','john_doe'),
+('25e23f5a-bebe-4764-9e7b-d8f2c38ce8b1','jane_smith'),
+('35e23f5a-bebe-4764-9e7b-d8f2c38ce8b2','alex_jones'),
+('faf6f7f9-d337-4680-9e9f-91a4e225586c','emily_wilson'),
+('04c92d1c-bbfa-48d4-a18f-9b6bb8af4f37','michael_clark'),
+('d276adce-71e6-4aff-8476-e539704e4fb1','sarah_brown'),
+('afb7055b-184b-41d7-88ae-ac7367cfb6cf','david_thomas'),
+('c20cf0e4-8132-4384-88fc-1c1a3c2eb6c1','laura_martin'),
+('9ab47964-4b0a-4b17-87dd-965aa38ed718','chris_hill'),
+('e3cff5c4-caa2-4fc0-9d56-87131d551751','jessica_scott'),
+('d1338087-d0a3-47e4-9b89-99bf29e4212b','matt_wright'),
+('942300f3-bbe4-489a-bf1c-72068ef15d2c','olivia_green'),
+('6580061d-3c76-446e-9440-b47a472c1d90','ryan_evans'),
+('aade44df-1833-41f9-a038-c94b3bb94caa','hannah_king'),
+('0941585d-8e20-4612-a26f-b68755034baf','tom_hall'),
+('1e959812-61bc-4ec0-bacb-a3863f8f9347','lucy_adams'),
+('13223fea-de83-4f3e-aa26-02e5b30bdafc','peter_wilson'),
+('7445eaee-d1fb-49a2-96d0-ed10bcd59fc8','rachel_baker'),
+('d5a22087-8663-4798-a3da-812bbb324d84','jack_carter'),
+('220847b2-ea52-4dfa-828e-c644409c8e36','amy_morris');
+
+SELECT * FROM public."user";
+
+
+INSERT INTO public."gender" (name) VALUES
 ('Male'),
 ('Female'),
 ('Other');
 
-SELECT * FROM "gender";
+SELECT * FROM public."gender";
 
 
-INSERT INTO "profile" (user_id, birthdate, contact, address, gender_id, biography, avatar) VALUES
+INSERT INTO public."profile" (user_id, birthdate, contact, address, gender_id, biography, avatar) VALUES
 (1, '1990-01-01', '+123456789', 'Address 1', 1, 'Bio for user 1', 'avatar1.jpg'),
 (2, '1991-02-02', '+987654321', 'Address 2', 2, 'Bio for user 2', 'avatar2.jpg'),
 (3, '1992-03-03', '+111222333', 'Address 3', 1, 'Bio for user 3', 'avatar3.jpg'),
 (4, '1993-04-04', '+444555666', 'Address 4', 2, 'Bio for user 4', 'avatar4.jpg'),
 (5, '1994-05-05', '+777888999', 'Address 5', 1, 'Bio for user 5', 'avatar5.jpg');
 
-INSERT INTO "profile" (user_id, birthdate, contact, address, gender_id, biography, avatar) VALUES
+INSERT INTO public."profile" (user_id, birthdate, contact, address, gender_id, biography, avatar) VALUES
 (6, '1995-06-06', '+111111111', 'Address 6', 1, 'Bio for user 6', 'avatar6.jpg'),
 (7, '1996-07-07', '+222222222', 'Address 7', 2, 'Bio for user 7', 'avatar7.jpg'),
 (8, '1997-08-08', '+333333333', 'Address 8', 1, 'Bio for user 8', 'avatar8.jpg'),
@@ -73,16 +106,16 @@ INSERT INTO "profile" (user_id, birthdate, contact, address, gender_id, biograph
 (24, '2013-12-12', '+1919191919', 'Address 24', 2, 'Bio for user 24', 'avatar24.jpg'),
 (25, '2014-01-01', '+2020202020', 'Address 25', 1, 'Bio for user 25', 'avatar25.jpg');
 
-select * from "profile";
+select * from public."profile";
 
-INSERT INTO "garment_type" (name) VALUES
+INSERT INTO public."garment_type" (name) VALUES
 ('Shirt'),
 ('Pants'),
 ('Dress'),
 ('Skirt'),
 ('Jacket');
 
-INSERT INTO "garment_type" (name) VALUES
+INSERT INTO public."garment_type" (name) VALUES
 ('T-Shirt'),
 ('Blouse'),
 ('Shorts'),
@@ -102,18 +135,17 @@ INSERT INTO "garment_type" (name) VALUES
 ('Parka'),
 ('Vest');
 
-select * from garment_type;
+select * from public."garment_type";
 
 
-
-INSERT INTO garment (user_id, garment_type_id, photo, date_worn) VALUES
+INSERT INTO public."garment" (profile_id, garment_type_id, photo, date_worn) VALUES
 (1, 1, 'shirt1.jpg', '2024-01-01'),
 (2, 2, 'pants1.jpg', '2024-01-02'),
 (3, 3, 'dress1.jpg', '2024-01-03'),
 (4, 4, 'skirt1.jpg', '2024-01-04'),
 (5, 5, 'jacket1.jpg', '2024-01-05');
 
-INSERT INTO garment (user_id, garment_type_id, photo, date_worn) VALUES
+INSERT INTO garment (profile_id, garment_type_id, photo, date_worn) VALUES
 (6, 6, 'shirt2.jpg', '2024-01-06'),
 (7, 7, 'pants2.jpg', '2024-01-07'),
 (8, 8, 'dress2.jpg', '2024-01-08'),
@@ -135,17 +167,17 @@ INSERT INTO garment (user_id, garment_type_id, photo, date_worn) VALUES
 (24, 4, 'skirt5.jpg', '2024-01-24'),
 (25, 5, 'jacket5.jpg', '2024-01-25');
 
-select * from garment;
+select * from public."garment";
 
 
-INSERT INTO outfit_type (name) VALUES
+INSERT INTO public."outfit_type" (name) VALUES
 ('Casual'),
 ('Formal'),
 ('Sportswear'),
 ('Business'),
 ('Party');
 
-INSERT INTO outfit_type (name) VALUES
+INSERT INTO public."outfit_type" (name) VALUES
 ('Beachwear'),
 ('Athleisure'),
 ('Smart Casual'),
@@ -167,16 +199,17 @@ INSERT INTO outfit_type (name) VALUES
 ('Date Night'),
 ('Interview');
 
-select * from outfit_type;
+select * from public."outfit_type";
 
-INSERT INTO outfits (user_id, name, outfit_type_id, date_worn, cover_photo) VALUES
+
+INSERT INTO public."outfits" (profile_id, name, outfit_type_id, date_worn, cover_photo) VALUES
 (1, 'Outfit 1', 1, '2024-01-01', 'outfit1.jpg'),
 (2, 'Outfit 2', 2, '2024-01-02', 'outfit2.jpg'),
 (3, 'Outfit 3', 3, '2024-01-03', 'outfit3.jpg'),
 (4, 'Outfit 4', 4, '2024-01-04', 'outfit4.jpg'),
 (5, 'Outfit 5', 5, '2024-01-05', 'outfit5.jpg');
 
-INSERT INTO outfits (user_id, name, outfit_type_id, date_worn, cover_photo) VALUES
+INSERT INTO public."outfits" (profile_id, name, outfit_type_id, date_worn, cover_photo) VALUES
 (6, 'Outfit 6', 6, '2024-01-06', 'outfit6.jpg'),
 (7, 'Outfit 7', 7, '2024-01-07', 'outfit7.jpg'),
 (8, 'Outfit 8', 8, '2024-01-08', 'outfit8.jpg'),
@@ -198,12 +231,12 @@ INSERT INTO outfits (user_id, name, outfit_type_id, date_worn, cover_photo) VALU
 (24, 'Outfit 24', 4, '2024-01-24', 'outfit24.jpg'),
 (25, 'Outfit 25', 5, '2024-01-25', 'outfit25.jpg');
 
-select * from outfits;
+select * from public."outfits";
 
 
 -- TODO
 
-INSERT INTO garments_outfits (garment_id , outfit_id) VALUES
+INSERT INTO public."garments_outfits" (garment_id , outfit_id) VALUES
 (1, 1),
 (2, 2),
 (3, 3),
@@ -213,7 +246,7 @@ INSERT INTO garments_outfits (garment_id , outfit_id) VALUES
 (4, 4),
 (5, 5);
 
-INSERT INTO garments_outfits (garment_id , outfit_id) VALUES
+INSERT INTO public."garments_outfits" (garment_id , outfit_id) VALUES
 (6, 6),
 (7, 7),
 (8, 8),
@@ -265,10 +298,10 @@ INSERT INTO garments_outfits (garment_id , outfit_id) VALUES
 (9, 4),
 (10, 5);
 
-select * from garments_outfits;
+select * from public."garments_outfits";
 
 
-INSERT INTO followers (follower_id, following_id) VALUES
+INSERT INTO public."followers" (follower_id, following_id) VALUES
 (1, 2),
 (2, 3),
 (3, 4),
@@ -276,7 +309,7 @@ INSERT INTO followers (follower_id, following_id) VALUES
 (5, 1),
 (1, 5);
 
-INSERT INTO followers (follower_id, following_id) VALUES
+INSERT INTO public."followers" (follower_id, following_id) VALUES
 (6, 7),
 (7, 8),
 (8, 9),
@@ -298,17 +331,17 @@ INSERT INTO followers (follower_id, following_id) VALUES
 (24, 25),
 (25, 1);
 
-select * from followers;
+select * from public."followers";
 
 
-INSERT INTO ratings (outfit_id, user_id, rating) VALUES
+INSERT INTO public."ratings" (outfit_id, profile_id, rating) VALUES
 (1, 2, 4),
 (2, 3, 5),
 (3, 4, 3),
 (4, 5, 4),
 (5, 1, 5);
 
-INSERT INTO ratings (outfit_id, user_id, rating) VALUES
+INSERT INTO public."ratings" (outfit_id, profile_id, rating) VALUES
 (6, 7, 4),
 (7, 8, 3),
 (8, 9, 5),
@@ -330,23 +363,24 @@ INSERT INTO ratings (outfit_id, user_id, rating) VALUES
 (24, 25, 5),
 (25, 1, 4);
 
-INSERT INTO ratings (outfit_id, user_id, rating) VALUES
+INSERT INTO public."ratings" (outfit_id, profile_id, rating) VALUES
 (1, 2, 4),
 (1, 3, 5),
 (1, 4, 3),
 (1, 5, 4),
 (1, 1, 5);
 
-select * from ratings;
+select * from public."ratings";
 
-INSERT INTO comments (outfit_id, user_id, comment) VALUES
+
+INSERT INTO public."comments" (outfit_id, profile_id, comment) VALUES
 (1, 3, 'Nice outfit!'),
 (2, 4, 'Love it!'),
 (3, 5, 'Great style!'),
 (4, 1, 'I woulnd thought of this combo!'),
 (5, 2, 'Ok');
 
-INSERT INTO comments (outfit_id, user_id, comment) VALUES
+INSERT INTO public."comments" (outfit_id, profile_id, comment) VALUES
 (6, 7, 'Looking great!'),
 (7, 8, 'Fantastic choice!'),
 (8, 9, 'Stylish outfit!'),
@@ -369,11 +403,11 @@ INSERT INTO comments (outfit_id, user_id, comment) VALUES
 (25, 1, 'Impressive!');
 
 
-INSERT INTO comments (outfit_id, user_id, comment) VALUES
+INSERT INTO public."comments" (outfit_id, profile_id, comment) VALUES
 (1, 3, 'Nice outfit!'),
 (2, 4, 'Love it!'),
 (1, 5, 'Great style!'),
 (2, 1, 'I woulnd thought of this combo!'),
 (1, 2, 'Ok');
 
-select * from "comments";
+select * from public."comments";

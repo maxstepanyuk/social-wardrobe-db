50c2932793621f0101eda5b79cbb5329ff9cd072
Maks Step - Deb pc <40894200+maxstepanyuk@users.noreply.github.com>
2024-06-05 00:07:13 +0300

    coomented irrelevant features

--- functions.sql
+++ functions.sql
@@ -23,114 +23,96 @@ SELECT * FROM auth.uid();
 
 
 
-CREATE OR REPLACE FUNCTION public.get_user_id_by_auth_uuid(p_uuid UUID)
-RETURNS int
-LANGUAGE sql
-SECURITY DEFINER
-SET search_path = ''
-STABLE
-AS $$
-    SELECT public."user".user_id 
-    FROM public."user"
-    WHERE public."user".supabaseauth_user_id = p_uuid
-   ;
-$$;
-
-SELECT get_user_id_by_auth_uuid('6580061d-3c76-446e-9440-b47a472c1d90');
-SELECT get_user_id_by_auth_uuid(auth.uid());
-
-
-
-CREATE OR REPLACE FUNCTION public.get_current_user_id()
-RETURNS int
-LANGUAGE sql
-SECURITY DEFINER
-SET search_path = ''
-STABLE
-AS $$
-    SELECT public.get_user_id_by_uuid(auth.uid());
-$$;
-
-SELECT * FROM public.get_current_user_id();
-SELECT * FROM public.get_current_user_id(), auth.uid();
-
-
-
-CREATE OR REPLACE
-FUNCTION public.get_profile_id_by_user_id(u_id INT)
-RETURNS INT
-LANGUAGE SQL
-SECURITY DEFINER
-SET search_path = ''
-STABLE
-AS $$
-	SELECT	public."profile".profile_id
-	FROM	public."profile"
-	WHERE	public."profile".user_id = u_id
-;
-$$;
-
-SELECT * FROM public.get_profile_id_by_user_id(6);
-
-
-
-
-
-CREATE OR REPLACE
-FUNCTION get_curent_profile_id()
-RETURNS INT
-LANGUAGE SQL
-SECURITY DEFINER
-SET search_path = ''
-STABLE
-AS $$
-	SELECT public.get_profile_id_by_user_id(public.get_current_user_id());
-$$;
-
-SELECT * FROM get_curent_profile_id();
-
-SELECT * FROM get_curent_profile_id(), public.get_current_user_id(), auth.uid(); -- IF auth.uid() has random it gives all random EVERY time
-
-WITH uid AS (
-SELECT
-	auth.uid() AS uid
-)
-SELECT
-	*
-FROM
-	uid,
-	get_user_id_by_auth_uuid(uid),
-	public.get_profile_id_by_user_id(get_user_id_by_auth_uuid(uid))
-;
-
-SELECT
-    au.id AS auth_user_id,
---    u.email,
---    u.encrypted_password,
---    u.created_at AS auth_created_at,
-    pu.user_id,
---    pu.username,
-    pr.profile_id
---    ,
---    pr.birthdate,
---    pr.contact,
---    pr.address,
---    pr.gender_id,
---    pr.biography,
---    pr.avatar
-FROM
-    auth."users" au
-JOIN
-    public."user" pu ON au.id = pu.supabaseauth_user_id
-JOIN
-    public."profile" pr ON pu.user_id = pr.user_id
-WHERE
-    au.id = '7b75bd54-0e55-43e1-a2d9-20024c80fd9a'
-;
-
-
-
---get_user_id_by_auth_uuid
+--CREATE OR REPLACE FUNCTION public.get_user_id_by_auth_uuid(p_uuid UUID)
+--RETURNS int
+--LANGUAGE sql
+--SECURITY DEFINER
+--SET search_path = ''
+--STABLE
+--AS $$
+--    SELECT public."user".user_id 
+--    FROM public."user"
+--    WHERE public."user".supabaseauth_user_id = p_uuid
+--   ;
+--$$;
+--
+--SELECT get_user_id_by_auth_uuid('6580061d-3c76-446e-9440-b47a472c1d90');
+--SELECT get_user_id_by_auth_uuid(auth.uid());
+--
+--
+--
+--CREATE OR REPLACE FUNCTION public.get_current_user_id()
+--RETURNS int
+--LANGUAGE sql
+--SECURITY DEFINER
+--SET search_path = ''
+--STABLE
+--AS $$
+--    SELECT public.get_user_id_by_uuid(auth.uid());
+--$$;
+--
+--SELECT * FROM public.get_current_user_id();
+--
+---- Update the function get_profile_id_by_user_id to use supabaseauth_user_id instead of user_id
+--CREATE OR REPLACE FUNCTION public.get_profile_id_by_user_id(u_id INT)
+--RETURNS INT
+--LANGUAGE SQL
+--SECURITY DEFINER
+--SET search_path = ''
+--STABLE
+--AS $$
+--    SELECT public."profile".profile_id
+--    FROM public."profile"
+--    JOIN public."user" ON public."profile".supabaseauth_user_id = public."user".supabaseauth_user_id
+--    WHERE public."user".user_id = u_id;
+--$$;
+--
+---- Test the updated function
+--SELECT * FROM public.get_profile_id_by_user_id(6);
+--
+---- Update the function get_current_profile_id to use the updated functions and schema
+--CREATE OR REPLACE FUNCTION public.get_current_profile_id()
+--RETURNS INT
+--LANGUAGE SQL
+--SECURITY DEFINER
+--SET search_path = ''
+--STABLE
+--AS $$
+--    SELECT public.get_profile_id_by_user_id(public.get_current_user_id());
+--$$;
+--
+---- Test the updated function
+--SELECT * FROM public.get_current_profile_id();
+--
+---- Test the updated functions together
+--WITH uid AS (
+--    SELECT auth.uid() AS uid
+--)
+--SELECT 
+--    *,
+--    get_user_id_by_auth_uuid(uid) AS user_id,
+--    public.get_profile_id_by_user_id(get_user_id_by_auth_uuid(uid)) AS profile_id
+--FROM 
+--    uid;
+--
+---- Select user and profile data based on auth_user_id
+--SELECT
+--    au.id AS auth_user_id,
+--    pu.user_id,
+--    pr.profile_id
+--FROM
+--    auth."users" au
+--JOIN
+--    public."user" pu ON au.id = pu.supabaseauth_user_id
+--JOIN
+--    public."profile" pr ON pu.supabaseauth_user_id = pr.supabaseauth_user_id
+--WHERE
+--    au.id = '7b75bd54-0e55-43e1-a2d9-20024c80fd9a'
+--;
+--
+--
+--
+----get_user_id_by_auth_uuid
 
 
 
